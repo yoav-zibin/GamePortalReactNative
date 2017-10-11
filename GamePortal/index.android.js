c7985599621b926c64437b2d8d148ae6bdb9753f
samuelsmith <samuel.smith.hk@gmail.com>
2017-10-10 22:52:17 -0400

    merge5 - bringing in android code

--- GamePortal/index.android.js
+++ GamePortal/index.android.js
@@ -1,53 +1,110 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry, BackHandler, View
 } from 'react-native';
 
-export default class FirebaseLogin extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+import {
+    Provider
+} from 'react-redux';
+
+import * as firebase from 'firebase';
+
+import store from './src/store/configure_store';
+import styles from './src/styles/android_style';
+import { SplashContainer } from './src/android/containers/splash_container';
+import { LoginDefaultContainer } from './src/android/containers/login_default_container'
+import { HomeContainer } from './src/android/containers/home_container'
+import { LoginEmailContainer } from "./src/android/containers/login_email_container";
+import { CreateAccountContainer } from "./src/android/containers/create_account_container";
+
+
+import {switchScreen} from "./src/actions/screen_actions";
+
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDmRQIcz7cPJXu28k9bo43b9nqQCI4naxE",
+    authDomain: "universalgamemaker.firebaseapp.com",
+    databaseURL: "https://universalgamemaker.firebaseio.com",
+    storageBucket: "universalgamemaker.appspot.com"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default class GamePortalReactNative extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            screen: store.getState().screen.screen
+        };
+
+        store.subscribe(() => {
+           if (store.getState().screen.screen !== this.state.screen) {
+               this.setState({
+                   screen: store.getState().screen.screen
+               });
+           }
+        });
+    }
+
+    componentDidMount() {
+        BackHandler.addEventListener("hardwareBackPress", () => {
+            let screen = this.state.screen;
+            let newScreen = undefined;
+
+            switch (screen) {
+                case 'LoginDefault':
+                    newScreen = 'Splash';
+                    break;
+                case 'LoginEmail':
+                    newScreen = 'LoginDefault';
+                    break;
+                case 'CreateAccount':
+                    newScreen = 'LoginEmail';
+                    break;
+                case 'Home':
+                    newScreen = 'Splash';
+                    break;
+                case 'Splash':
+                    return false;
+            }
+
+            if (newScreen !== undefined) {
+                store.dispatch(switchScreen(newScreen))
+            }
+
+            return true;
+        });
+    }
+
+    renderContainer() {
+        let screen = this.state.screen;
+
+        switch (screen) {
+            case 'LoginDefault':
+                return (<LoginDefaultContainer />);
+            case 'LoginEmail':
+                return (<LoginEmailContainer />);
+            case 'CreateAccount':
+                return (<CreateAccountContainer />);
+            case 'Home':
+                return (<HomeContainer />);
+            default:
+                return (<SplashContainer/>);
+        }
+    }
+
+    render() {
+        return (
+            <Provider store={store}>
+                <View style={styles.container}>
+                    {this.renderContainer()}
+                </View>
+            </Provider>
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('FirebaseLogin', () => FirebaseLogin);
+AppRegistry.registerComponent('GamePortalReactNative', () => GamePortalReactNative);
