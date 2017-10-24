39ea3d29a98f811dd97a7a12d8fd7215bff8a402
samuelsmith <samuel.smith.hk@gmail.com>
2017-10-24 15:58:58 -0400

    Showing list of active users in tab

--- GamePortal/src/frontend/containers/home_container.js
+++ GamePortal/src/frontend/containers/home_container.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 import { switchScreen, setLoading } from "../actions/screen_actions";
 import { setLoggedOut } from "../actions/user_actions";
 import { switchTab } from "../actions/home_actions";
+import { setRecentlyConnectedUsers } from "../actions/recently_connected_actions";
 
 import HomeComponent from '../components/home_component'
 
@@ -10,14 +11,16 @@ const mapStateToProps = state => ({
     username: state.user.username,
     avatarURL: state.user.avatarURL,
     loading: state.screen.loading,
-    tab: state.home.tab
+    tab: state.home.tab,
+    recentlyConnectedUsers: state.recentlyConnected.users
 });
 
 const mapDispatchToProps = dispatch => ({
     switchScreen: screen => dispatch(switchScreen(screen)),
     switchTab: tab => dispatch(switchTab(tab)),
     setLoading: loading => dispatch(setLoading(loading)),
-    setLoggedOut: () => dispatch(setLoggedOut())
+    setLoggedOut: () => dispatch(setLoggedOut()),
+    setRecentlyConnectedUsers: user => dispatch(setRecentlyConnectedUsers(user)),
 });
 
 export const HomeContainer = connect(mapStateToProps, mapDispatchToProps)(HomeComponent);
